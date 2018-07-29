09a066a34000204d1f686426f811235662e917a0
Salman Husain <shusain93@gmail.com>
2018-07-28 21:57:19 -0600

    Fixed edited attributed on Link

--- framework/Model/Link.swift
+++ framework/Model/Link.swift
@@ -149,7 +149,7 @@ public struct Link: Thing, Created, Votable {
     /**
     example: false
     */
-    public var edited: Bool
+    public var edited: Int
     /**
     the CSS class of the link's flair.
     example:
@@ -270,7 +270,7 @@ public struct Link: Thing, Created, Votable {
         hidden = false
         thumbnail = ""
         subredditId = ""
-        edited = false
+        edited = -1
         linkFlairCssClass = ""
         authorFlairCssClass = ""
         downs = 0
@@ -331,7 +331,7 @@ public struct Link: Thing, Created, Votable {
         hidden = data["hidden"] as? Bool ?? false
         thumbnail = convertObjectToEscapedURLString(data["thumbnail"])
         subredditId = data["subreddit_id"] as? String ?? ""
-        edited = data["edited"] as? Bool ?? false
+        edited = data["edited"] as? Int ?? -1
         linkFlairCssClass = data["link_flair_css_class"] as? String ?? ""
         authorFlairCssClass = data["author_flair_css_class"] as? String ?? ""
         downs = data["downs"] as? Int ?? 0
